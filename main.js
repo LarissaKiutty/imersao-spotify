1d02120d94cb3ff004a65a9f89a1ff3b2ee78ccd
LarissaKiutty <larissa.souza1941@gmail.com>
2024-05-19 11:45:52 -0300

    FEAT: starting the JavaScript file, utilizing the DOM, and creating a JSON file.

---
+++ main.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById('search-input');
+const resultArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+  resultPlaylist.classList.add('hidden');
+  const artistName = document.getElementById('artist-name');
+  const artisImage = document.getElementById('artist-img');
+
+  result.forEach(element => {
+    artistName.innerText - element.name;
+    artisImage.src = element.urlImg;
+  })
+
+  resultArtists.classList.remove('hidden');
+}
+
+document.addEventListener('input', function() {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  if(searchTerm === '') {
+    resultPlaylist.classList.add('hidden');
+    resultArtists.classList.remove('hidden');
+    return;
+  }
+
+  requestApi(searchTerm);
+});
