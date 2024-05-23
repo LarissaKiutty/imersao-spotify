c22cd0e15d6a6f5d9e4d28052d157636f7bc987c
LarissaKiutty <larissa.souza1941@gmail.com>
2024-05-23 20:57:24 -0300

    FIX: requestApi search fixed and CSS improvements.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const resultArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+  const url = `http://localhost:3000/artists?name=${searchTerm}`;
   fetch(url)
     .then((response) => response.json())
     .then((result) => displayResults(result))
@@ -15,7 +15,7 @@ function displayResults(result) {
   const artisImage = document.getElementById('artist-img');
 
   result.forEach(element => {
-    artistName.innerText - element.name;
+    artistName.innerText = element.name;
     artisImage.src = element.urlImg;
   })
 
@@ -23,7 +23,7 @@ function displayResults(result) {
 }
 
 document.addEventListener('input', function() {
-  const searchTerm = searchInput.value.toLowerCase();
+  const searchTerm = searchInput.value;
 
   if(searchTerm === '') {
     resultPlaylist.classList.add('hidden');
